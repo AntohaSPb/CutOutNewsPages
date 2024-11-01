35337eeb6fde51b98f82fd81f4943ef0680bd8b8
AntohaSPb <anton.b.ivanov@gmail.com>
2024-11-01 20:45:42 +0300

    Update CutPage_20241028.py

--- CutPage_20241028.py
+++ CutPage_20241028.py
@@ -1,3 +1,4 @@
+import os
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
@@ -14,11 +15,16 @@ import segmentation_models_pytorch as smp
 from segmentation_models_pytorch import utils
 
 used_model = 'Resnet'  #or 'Unet'
-norm_proc = 'numpy_stdev' # or 'torch' or 'numpy_minmax'
+norm_proc = 'numpy_minmax' # or 'torch' or 'numpy_stdev' or 'none'
+
+in_folder = 'c:/Pyproj/Segm/Code/imagery/Test2'  # Replace with your image path
+out_folder = 'c:/Pyproj/Segm/Code/imagery/Test2/Out'  # Replace with your output path
+
+need_show = True
 
 if used_model == 'Unet':
     #inference code for U-Net
-    best_model = torch.jit.load('c:/Pyproj/Segm/Code/models/best_model_new2.pt', map_location='cpu')
+    best_model = torch.jit.load('c:/Pyproj/Segm/Code/models/best_model_new.pt', map_location='cpu')
 
 if used_model == 'Resnet':
     #inference code for ResNet
@@ -30,141 +36,166 @@ if used_model == 'Resnet':
 #set whatever model to evaluation mode, no back prop of err
 best_model.eval()
 
-im_path = 'c:/Pyproj/Segm/Code/imagery/Test/IMG-081.JPG'  # Replace with your image path
-ou_path = 'c:/Pyproj/Segm/Code/imagery/Crop/DSC0395.JPG'  # Replace with your output path
-ppage = cv2.imread(im_path)
+for filename in os.listdir(in_folder):
+    # Construct full file path
+    im_path = os.path.join(in_folder, filename)
+    ou_path = os.path.join(out_folder, filename)
 
-if ppage is None:
-    print("Error: Image not loaded. Please check the file path.")
-    
-#turn BGR into RGB
-ppageRGB = cv2.cvtColor(ppage, cv2.COLOR_BGR2RGB)
+    #im_path = 'c:/Pyproj/Segm/Code/imagery/Test/Source.jpg'  # Replace with your image path
+    #ou_path = 'c:/Pyproj/Segm/Code/imagery/Crop/DSC0395.JPG'  # Replace with your output path
+    ppage = cv2.imread(im_path)
 
-#detect original pixels of the page
-page_size = ppageRGB.shape
-print ("original size" , page_size)
+    if ppage is None:
+        print("Error: Image not loaded. Please check the file path.")
+        
+    #turn BGR into RGB
+    ppageRGB = cv2.cvtColor(ppage, cv2.COLOR_BGR2RGB)
 
-#resize and show new dims
-small_size = (384, 256)  # Note: OpenCV uses (width, height) format
-ppage_small = cv2.resize(ppageRGB, small_size)
-page_size = ppage_small.shape
-print ("reduced size", page_size)
+    #detect original pixels of the page
+    page_size = ppageRGB.shape
+    print ("original size" , page_size)
 
-#work on the page to have it blank
-#kernel = np.ones((3,3),np.uint8)
-# ppage_small_old =  np.copy(ppage_small) 
-# cv2.morphologyEx(ppage_small, cv2.MORPH_CLOSE, kernel, iterations= 3)
+    #resize and show new dims
+    small_size = (384, 256)  # Note: OpenCV uses (width, height) format
+    ppage_small = cv2.resize(ppageRGB, small_size)
+    page_size = ppage_small.shape
+    print ("reduced size", page_size)
 
-#normalize and produce a tensor #RGB 0-255 range into 0-1 tensor, HxWxC to CxHxW and add batch dimension
-if norm_proc == 'numpy_minmax':
-    #normalize the image using NumPy with min-max filter
-    min_val = ppage_small.min(axis=(0, 1))
-    max_val = ppage_small.max(axis=(0, 1))
-    ppage_norm = (ppage_small - min_val) / (max_val - min_val)
-    ppage_tensor = torch.from_numpy(ppage_norm).permute(2, 0, 1).unsqueeze(0).float()
-    mystd, mymean = (0.5, 0.5, 0.5), (0.5, 0.5, 0.5)
+    #work on the page to have it blank
+    #kernel = np.ones((3,3),np.uint8)
+    # ppage_small_old =  np.copy(ppage_small) 
+    # cv2.morphologyEx(ppage_small, cv2.MORPH_CLOSE, kernel, iterations= 3)
 
-if norm_proc == 'numpy_stdev':
-    #normalize the image using NumPy with mean and stdev
-    mymean = np.mean(ppage_small, axis=(0, 1))   #  (0.4611, 0.4359, 0.3905)
-    mystd = np.std(ppage_small, axis=(0, 1))     #  (0.2193, 0.2150, 0.2109)
-    
-    #norm option 1: basic standardization around mean as zero, negatives clamped when shown
-    #ppage_norm = ((ppage_small - mymean) / mystd)
-    #norm option 2: set the picture within the double sigma across the mean, tails clipped
-    ppage_norm = np.clip(((ppage_small - (mymean - mystd))/(mystd * 2)), 0, 1)
+    #normalize and produce a tensor #RGB 0-255 range into 0-1 tensor, HxWxC to CxHxW and add batch dimension
+    if norm_proc == 'numpy_minmax':
+        #normalize the image using NumPy with min-max filter
+        min_val = ppage_small.min(axis=(0, 1))
+        max_val = ppage_small.max(axis=(0, 1))
+        ppage_norm = (ppage_small - min_val) / (max_val - min_val)
+        ppage_tensor = torch.from_numpy(ppage_norm).permute(2, 0, 1).unsqueeze(0).float()
+        mystd, mymean = (0.5, 0.5, 0.5), (0.5, 0.5, 0.5)
 
-    ppage_tensor = torch.from_numpy(ppage_norm).permute(2, 0, 1).unsqueeze(0).float()
-    print (mymean, mystd)
+    if norm_proc == 'numpy_stdev':
+        #normalize the image using NumPy with mean and stdev
+        mymean = np.mean(ppage_small, axis=(0, 1))   #  (0.4611, 0.4359, 0.3905)
+        mystd = np.std(ppage_small, axis=(0, 1))     #  (0.2193, 0.2150, 0.2109)
+        
+        #norm option 1: basic standardization around mean as zero, negatives clamped when shown
+        #ppage_norm = ((ppage_small - mymean) / mystd)
+        #norm option 2: set the picture within the double sigma across the mean, tails clipped
+        ppage_norm = np.clip(((ppage_small - (mymean - mystd))/(mystd * 2)), 0, 1)
 
-if norm_proc == 'torch':
-    #normalize using torchvision
-    mymean =  (0.4611, 0.4359, 0.3905)      #from source dataset
-    mystd =  (0.2193, 0.2150, 0.2109)       #from source dataset
-    normalize = transforms.Compose([transforms.ToTensor(), transforms.Normalize(mymean, mystd),])
-    ppage_tnorm = normalize(ppage_small)    #execute function resulting in a tensor
-    ppage_tensor = torch.unsqueeze(ppage_tnorm, dim=0).float()  #expand tensor with batch dimension
-    ppage_norm = ppage_tnorm.permute(1, 2, 0).squeeze().numpy() #tensor to numpy for showing
+        ppage_tensor = torch.from_numpy(ppage_norm).permute(2, 0, 1).unsqueeze(0).float()
+        print (mymean, mystd)
 
-page_size = ppage_tensor.shape
-print ("supplied tensor size", page_size)
+    if norm_proc == 'torch':
+        #normalize using torchvision
+        mymean =  (0.4611, 0.4359, 0.3905)      #from source dataset
+        mystd =  (0.2193, 0.2150, 0.2109)       #from source dataset
+        normalize = transforms.Compose([transforms.ToTensor(), transforms.Normalize(mymean, mystd),])
+        ppage_tnorm = normalize(ppage_small)    #execute function resulting in a tensor
+        ppage_tensor = torch.unsqueeze(ppage_tnorm, dim=0).float()  #expand tensor with batch dimension
+        ppage_norm = ppage_tnorm.permute(1, 2, 0).squeeze().numpy() #tensor to numpy for showing
 
-#supply my tensor to the model
-with torch.no_grad():  # Disable gradient calculation for inference
-    output = best_model(ppage_tensor)
+    if norm_proc == 'none':
+        ppage_norm = ppage_small
+        ppage_tensor = torch.from_numpy(ppage_norm).permute(2, 0, 1).unsqueeze(0).float()
+
+    page_size = ppage_tensor.shape
+    print ("supplied tensor size", page_size)
+
+    #supply my tensor to the model
+    with torch.no_grad():  # Disable gradient calculation for inference
+        output = best_model(ppage_tensor)
+        if used_model == 'Resnet':
+            pred_mask = output['out']        #ResNet produces a Dictionary with one more dimension for 'words'
+        if used_model == 'Unet':
+            pred_mask = output              #Unet gives out a tensor
+
+    #check out for prediction result    
+        page_size = pred_mask.shape
+        print ("returned mask size", page_size)
+
     if used_model == 'Resnet':
-        pred_mask = output['out']        #ResNet produces a Dictionary with one more dimension for 'words'
+        # kill batch dim of size 1, detach tensor from backwards propagation, turn into a NumPy array to show
+        np_mask = pred_mask.detach().squeeze().numpy()
+        page_size = np_mask.shape
+        print ("squeezed mask size", page_size)
+        #get highest probability class index, given that class axis is 0 (first)
+        sel_mask = np.argmax(np_mask, axis=0)
+        page_size = sel_mask.shape
+        print ("likely mask size", page_size)
+        #convert mask for manipulations
+        likely_mask = sel_mask.astype(np.uint8)
+
     if used_model == 'Unet':
-        pred_mask = output              #Unet gives out a tensor
+        # kill batch dim=1, detach tensor, shift classes to axis=2, turn into numpy array
+        np_mask = pred_mask.squeeze().permute(1, 2, 0).numpy()
+        #select my mask class
+        sel_mask = np_mask[:, :, 0]  # shape (256, 384)
+        #filter out pixels having below 50% prob of req obj type (i.e. all wrong objects)
+        likely_mask = (sel_mask > 0.9).astype(np.uint8) * 255  # shape (256, 384)
 
-#check out for prediction result    
-    page_size = pred_mask.shape
-    print ("returned mask size", page_size)
+    #manipulate with erosion and dilation
+    #Define a kernel
+    kernel = np.ones((5, 5), np.uint8)
+    #Erode the mask to remove small islands, Dilate the mask to restore the main object size
+    eroded_mask = cv2.erode(likely_mask, kernel, iterations=1)
+    smoothed_mask = cv2.dilate(eroded_mask, kernel, iterations=1)
+    # Apply opening to remove small islands,Apply closing to fill small holes
+    opened_mask = cv2.morphologyEx(likely_mask, cv2.MORPH_OPEN, kernel)
+    closed_mask = cv2.morphologyEx(opened_mask, cv2.MORPH_CLOSE, kernel)
+    # return adjusted image SMOOTHED
+    fixed_mask = smoothed_mask
 
-# kill batch dim of size 1, detach tensor from backwards propagation, turn into a NumPy array to show
-np_mask = pred_mask.detach().squeeze().numpy()
-page_size = np_mask.shape
-print ("squeezed mask size", page_size)
+    #smoothed contours
+    contours, _ = cv2.findContours(fixed_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    zero_mask = np.zeros_like(fixed_mask)
+    ppage_combi = np.copy(ppage_small)
 
-#get highest probability class index, given that class axis is 0 (first)
-likely_mask = np.argmax(np_mask, axis=0)
-page_size = likely_mask.shape
-print ("likely mask size", page_size)
+    if contours:
+        max_contour = max(contours, key=cv2.contourArea)   #get biggest contour
+        epsilon = 0.05 * cv2.arcLength(max_contour, True)  # accuracy for waste disposal
+        mypoly = cv2.approxPolyDP(max_contour, epsilon, True) #approximate with a polygon
+        cv2.drawContours(zero_mask, [mypoly], -1, (1), thickness=5) #draw on a new array
+        cv2.drawContours(ppage_combi, [mypoly], -1, (255, 0, 0), thickness=2) #draw on the image
 
-#convert mask for manipulations
-likely_mask = likely_mask.astype(np.uint8)
+    #for quadrilineal find the corners for transformation
+    if len(mypoly) == 4:
+        # Sort the points as top-left, top-right, bottom-right, bottom-left and put into an src array
+        mycorners = mypoly.reshape(4, 2)
+        sorted_points = sorted(mycorners, key=lambda x: (x[1], x[0]))
+        top_points = sorted(sorted_points[:2], key=lambda x: x[0])  # Top-left and top-right
+        bottom_points = sorted(sorted_points[2:], key=lambda x: x[0])  # Bottom-left and bottom-right
+        src_points = np.array([top_points[0], top_points[1], bottom_points[1], bottom_points[0]], dtype='float32')    
 
-#manipulate with erosion and dilation
-#Define a kernel
-kernel = np.ones((5, 5), np.uint8)
-#Erode the mask to remove small islands, Dilate the mask to restore the main object size
-eroded_mask = cv2.erode(likely_mask, kernel, iterations=1)
-smoothed_mask = cv2.dilate(eroded_mask, kernel, iterations=1)
-# Apply opening to remove small islands,Apply closing to fill small holes
-opened_mask = cv2.morphologyEx(likely_mask, cv2.MORPH_OPEN, kernel)
-closed_mask = cv2.morphologyEx(opened_mask, cv2.MORPH_CLOSE, kernel)
-# return adjusted image SMOOTHED
-fixed_mask = smoothed_mask
+    #define dest size and transform the image
+    width, height = 384, 256
+    #perspective transformation matrix: topleft, topright, botright, botleft
+    dst_points = np.float32([[0, 0], [width, 0], [width, height], [0, height]]) 
+    #estimate the matrix and apply it on image
+    matrix = cv2.getPerspectiveTransform(src_points, dst_points)
+    ppage_unwarp = cv2.warpPerspective(ppage_combi, matrix, (width, height))
 
-#smoothed contours
-contours, _ = cv2.findContours(fixed_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-zero_mask = np.zeros_like(fixed_mask)
-ppage_combi = np.copy(ppage_small)
+    if need_show == True:
+        #plot the images
+        images = [ppage_small, ppage_norm, likely_mask, fixed_mask, ppage_combi, ppage_unwarp]
+        titles = ['Resize', 'Normalize', 'Prediction', 'Smooth', 'Rectangle','Unwarp']
+        fig, axes = plt.subplots(2, 3, figsize=(10, 10))
+        axes = axes.flatten()
+        for i, ax in enumerate(axes):
+            ax.imshow(images[i], cmap='gray')  # Display image
+            ax.set_title(titles[i])  # Set title for each subplot
+            ax.axis('off')
+        plt.tight_layout()
+        plt.show()
 
-if contours:
-    max_contour = max(contours, key=cv2.contourArea)   #get biggest contour
-    epsilon = 0.05 * cv2.arcLength(max_contour, True)  # accuracy for waste disposal
-    mypoly = cv2.approxPolyDP(max_contour, epsilon, True) #approximate with a polygon
-    cv2.drawContours(zero_mask, [mypoly], -1, (1), thickness=5) #draw on a new array
-    cv2.drawContours(ppage_combi, [mypoly], -1, (255, 0, 0), thickness=2) #draw on the image
+    #write the output file
+    success = cv2.imwrite(ou_path, ppage_combi)
+    # Optional: Check if the image was saved successfully
+    if success:
+        print(f'Image saved successfully at {ou_path}')
+    else:
+        print('Failed to save the image.')
 
-#for quadrilineal find the corners for transformation
-if len(mypoly) == 4:
-    # Sort the points as top-left, top-right, bottom-right, bottom-left and put into an src array
-    mycorners = mypoly.reshape(4, 2)
-    sorted_points = sorted(mycorners, key=lambda x: (x[1], x[0]))
-    top_points = sorted(sorted_points[:2], key=lambda x: x[0])  # Top-left and top-right
-    bottom_points = sorted(sorted_points[2:], key=lambda x: x[0])  # Bottom-left and bottom-right
-    src_points = np.array([top_points[0], top_points[1], bottom_points[1], bottom_points[0]], dtype='float32')    
-
-#define dest size and transform the image
-width, height = 384, 256
-#perspective transformation matrix: topleft, topright, botright, botleft
-dst_points = np.float32([[0, 0], [width, 0], [width, height], [0, height]]) 
-#estimate the matrix and apply it on image
-matrix = cv2.getPerspectiveTransform(src_points, dst_points)
-ppage_unwarp = cv2.warpPerspective(ppage_combi, matrix, (width, height))
-
-#plot the images
-images = [ppage_small, ppage_norm, likely_mask, fixed_mask, ppage_combi, ppage_unwarp]
-titles = ['Resize', 'Normalize', 'Prediction', 'Smooth', 'Rectangle','Unwarp']
-fig, axes = plt.subplots(2, 3, figsize=(10, 10))
-axes = axes.flatten()
-for i, ax in enumerate(axes):
-    ax.imshow(images[i], cmap='gray')  # Display image
-    ax.set_title(titles[i])  # Set title for each subplot
-    ax.axis('off')
-plt.tight_layout()
-plt.show()
-
-del ppage, ppage_norm, likely_mask, np_mask, ppage_small, ppageRGB, ppage_tensor, ppage_combi
+    del ppage, ppage_norm, likely_mask, np_mask, ppage_small, ppageRGB, ppage_tensor, ppage_combi, sel_mask
